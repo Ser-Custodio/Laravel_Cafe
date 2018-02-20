aed2355929629f03b3960dd375b3c664c58b8ac8
Sergio Custodio <sergio.custodio@Digital-Grenoble.local>
2018-02-20 14:23:11 +0100

    Version Formulaire

---
+++ resources/views/front_office/mach.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Machine à Cafés</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+<div class="container">
+    <form method="post" action="{{ route('ventes.store') }}">
+    <div class='row'>
+
+            {{ csrf_field() }}
+
+            <div class='col-ms-2'>
+                <label><input type="radio" name="drink" value="1"><img src="img/Vue1/Choixboisson_tea.png" class="btnThe"></label>
+            </div>
+            <div class="col-ms-2">
+                <label>
+                    <input class="choiceSugar" type="number" name="nbSugar" min="0" max="5">
+                </label>
+                <img class="acucarMoins btnMoins" src="img/Vue1/Selection_sucre/button---inactive.png" >
+            </div>
+            <div class="col-ms-2">
+                <img class="acucarPlus btnPlus" src="img/Vue1/Selection_sucre/buttonPlusinactive.png">
+
+            </div>
+    </div>
+    <div><label>
+            <input type="radio" name="drink" value="2"><img src="img/Vue1/Choixboisson_Chocolat.png" class="btnChoc">
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="radio" name="drink" value="3"><img src="img/Vue1/Choixboisson_Latte.png" class="btnLat">
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="radio" name="drink" value="4"><img src="img/Vue1/Choixboisson_expresso.png" class="btnExp">
+        </label>
+    </div>
+        <button class="btn-valider btn btn-danger btn-lg">Valider</button>
+
+    <div class='nbsugar container'>
+        <img class="sugar1" src="img/Vue1/Selection_sucre/1_sucre.png">
+        <img class="sugar2" src="img/Vue1/Selection_sucre/2_sucres.png">
+        <img class="sugar3" src="img/Vue1/Selection_sucre/3_sucres.png">
+        <img class="sugar4" src="img/Vue1/Selection_sucre/4_sucres.png">
+        <img class="sugar5" src="img/Vue1/Selection_sucre/5_sucres.png">
+    </div>
+    </form>
+    <div class='coins container'>
+        <div class='row'>
+            <div class='col-ms-2'>
+                <input type='image' class='2euros' src='img/Vue1/2euros_selec.png' disabled="true">
+                <input type='image' class='1euro' src='img/Vue1/1euro_selec.png' disabled="true">
+            </div>
+            <div class='col-ms-2'>
+                <input type='image' class='50cts' src='img/Vue1/50cen_selec.png' disabled="true">
+                <input type='image' class='20cts' src='img/Vue1/20cen_selec.png' disabled="true">
+            </div>
+            <div class='col-ms-2'>
+                <input type='image' class='10cts' src='img/Vue1/10cen_selec.png' disabled="true">
+                <input type='image' class='5cts' src='img/Vue1/5cen_selec.png' disabled="true">
+            </div>
+        </div>
+    </div>
+    <div class='container'>
+        <input type='image' class='reset' src="img/Vue1/button-cancel.png">
+    </div>
+    <div class='row'>
+        <div class='messageBoisson'>
+            <p class="boiChoi"></p>
+            <p class='infoPiecesRendues'></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class='messagePieces'>
+            <p class='affPieces'></p>
+        </div>
+    </div>
+    <div class='row'>
+        <div class='messageRendu'>
+            <p class="monnaie"></p>
+        </div>
+    </div>
+    <div class='maintenance container'>
+        @if(\Illuminate\Support\Facades\Auth::check())
+            <a href="{{route('ventes.index')}}"> <input type='image' class='mtnance' src='img/Vue1/btn-mtn.png'></a>
+        @else
+            <a href="{{url('login')}}"> <input type='image' class='mtnance' src='img/Vue1/btn-mtn.png'></a>
+        @endif
+    </div>
+    <div class='monnayeur container'>
+        <img src="img/Vue1/rendu-monnaie.png" alt="monnayeur">
+    </div>
+    {{--<form method="post" action="{{ route('ventes.store') }}">--}}
+        {{--{{ csrf_field() }}--}}
+       {{----}}
+    {{--</form>--}}
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 drink">
+            <img id="Mug" class="boisson" src="img/drink/Mug.png" alt="mug">
+            <img id="espresso" class="boisson" src="img/drink/espresso.gif" alt="espresso">
+            <img id="latte" class="boisson" src="img/drink/Latte.gif" alt="Latte">
+            <img id="tea" class="boisson" src="img/drink/Tea.gif" alt="Tea">
+            <img id="chocolate" class="boisson" src="img/drink/Chocolate.gif" alt="Chocolate">
+
+        </div>
+
+    </div>
+
+</div>
+
+<footer>
+</footer>
+</body>
+</html>
--- resources/views/front_office/machine.blade.php
+++ resources/views/front_office/machine.blade.php
@@ -1,125 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Machine à Cafés</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js"></script>
-</head>
-<body>
-<div class="container">
-    <form method="post" action="{{ route('ventes.store') }}">
-    <div class='row'>
+@extends('templates/structure')
 
-            {{ csrf_field() }}
+@section('title')
+Machine à Café
+@endsection
 
-            <div class='col-ms-2'>
-                <label><input type="radio" name="drink" value="1"><img src="img/Vue1/Choixboisson_tea.png" class="btnThe"></label>
-            </div>
-            <div class="col-ms-2">
-                <label>
-                    <input class="choiceSugar" type="number" name="nbSugar" min="0" max="5">
-                </label>
-                <img class="acucarMoins btnMoins" src="img/Vue1/Selection_sucre/button---inactive.png" >
-            </div>
-            <div class="col-ms-2">
-                <img class="acucarPlus btnPlus" src="img/Vue1/Selection_sucre/buttonPlusinactive.png">
+@section('TitlePageName')
+Choisissez votre Boisson
+@endsection
 
-            </div>
-    </div>
-    <div><label>
-            <input type="radio" name="drink" value="2"><img src="img/Vue1/Choixboisson_Chocolat.png" class="btnChoc">
-        </label>
-    </div>
-    <div>
-        <label>
-            <input type="radio" name="drink" value="3"><img src="img/Vue1/Choixboisson_Latte.png" class="btnLat">
-        </label>
-    </div>
-    <div>
-        <label>
-            <input type="radio" name="drink" value="4"><img src="img/Vue1/Choixboisson_expresso.png" class="btnExp">
-        </label>
-    </div>
-        <button class="btn-valider btn btn-danger btn-lg">Valider</button>
-
-    <div class='nbsugar container'>
-        <img class="sugar1" src="img/Vue1/Selection_sucre/1_sucre.png">
-        <img class="sugar2" src="img/Vue1/Selection_sucre/2_sucres.png">
-        <img class="sugar3" src="img/Vue1/Selection_sucre/3_sucres.png">
-        <img class="sugar4" src="img/Vue1/Selection_sucre/4_sucres.png">
-        <img class="sugar5" src="img/Vue1/Selection_sucre/5_sucres.png">
-    </div>
+@section('content')
+<div class='displays formula'>
+    <form class="col-md-offset-2 col-md-8" method="post" action="{{ route('ventes.store') }}">
+        {{ csrf_field() }}
+        Available Drink: <br>
+        <div class="row table-responsive">
+            <table class="table table-editBoissons table-hover table-bordered col-sm-12">
+                <tr>
+                    <th class="col-md-2">NAME</th>
+                    <th class="col-md-2">PRICE</th>
+                    <th class="col-md-2">CHOIX</th>
+                </tr>
+                @foreach ($boisson as $uneBoisson)
+                @if($uneBoisson->dispo === true && $uneBoisson->active === 1)
+                <tr>
+                    <td>{{ $uneBoisson->name }} </td>
+                    <td>{{ number_format($uneBoisson->price/100,2) }}€</td>
+                    <td><input type="radio" name="drink" value="{{ $uneBoisson->id }}"></td>
+                </tr>
+                @endif
+                @endforeach
+            </table>
+        </div>
+        <br>
+        Number of Sugar:<br>
+        <div class="table-responsive">
+            <table class="table tableSugar">
+                <tr>
+                    @for($i =0; $i <= $nbSugar && $i < 6; $i++)
+                    <th class="col-md-2">{{ $i }}</th>
+                    @endfor
+                </tr>
+                <tr>
+                    @for($i =0; $i <= $nbSugar && $i < 6; $i++)
+                    <td class="col-md-2"><input type="radio" name="nbSugar" value={{$i}}>
+                    </td>
+                    @endfor
+                </tr>
+            </table>
+        </div>
+        <br>
+        <button class='btn btn-lg btn-success' type="submit">Commander</button>
     </form>
-    <div class='coins container'>
-        <div class='row'>
-            <div class='col-ms-2'>
-                <input type='image' class='2euros' src='img/Vue1/2euros_selec.png' disabled="true">
-                <input type='image' class='1euro' src='img/Vue1/1euro_selec.png' disabled="true">
-            </div>
-            <div class='col-ms-2'>
-                <input type='image' class='50cts' src='img/Vue1/50cen_selec.png' disabled="true">
-                <input type='image' class='20cts' src='img/Vue1/20cen_selec.png' disabled="true">
-            </div>
-            <div class='col-ms-2'>
-                <input type='image' class='10cts' src='img/Vue1/10cen_selec.png' disabled="true">
-                <input type='image' class='5cts' src='img/Vue1/5cen_selec.png' disabled="true">
-            </div>
-        </div>
-    </div>
-    <div class='container'>
-        <input type='image' class='reset' src="img/Vue1/button-cancel.png">
-    </div>
-    <div class='row'>
-        <div class='messageBoisson'>
-            <p class="boiChoi"></p>
-            <p class='infoPiecesRendues'></p>
-        </div>
-    </div>
-    <div class="row">
-        <div class='messagePieces'>
-            <p class='affPieces'></p>
-        </div>
-    </div>
-    <div class='row'>
-        <div class='messageRendu'>
-            <p class="monnaie"></p>
-        </div>
-    </div>
-    <div class='maintenance container'>
-        @if(\Illuminate\Support\Facades\Auth::check())
-            <a href="{{route('ventes.index')}}"> <input type='image' class='mtnance' src='img/Vue1/btn-mtn.png'></a>
-        @else
-            <a href="{{url('login')}}"> <input type='image' class='mtnance' src='img/Vue1/btn-mtn.png'></a>
-        @endif
-    </div>
-    <div class='monnayeur container'>
-        <img src="img/Vue1/rendu-monnaie.png" alt="monnayeur">
-    </div>
-    {{--<form method="post" action="{{ route('ventes.store') }}">--}}
-        {{--{{ csrf_field() }}--}}
-       {{----}}
-    {{--</form>--}}
 </div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-4 drink">
-            <img id="Mug" class="boisson" src="img/drink/Mug.png" alt="mug">
-            <img id="espresso" class="boisson" src="img/drink/espresso.gif" alt="espresso">
-            <img id="latte" class="boisson" src="img/drink/Latte.gif" alt="Latte">
-            <img id="tea" class="boisson" src="img/drink/Tea.gif" alt="Tea">
-            <img id="chocolate" class="boisson" src="img/drink/Chocolate.gif" alt="Chocolate">
-
-        </div>
-
-    </div>
-
-</div>
-
-<footer>
-</footer>
-</body>
-</html>
+@endsection
