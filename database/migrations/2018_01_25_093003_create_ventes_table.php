f7774e561c4c8a19cf42ecd6d90052f440f8373a
Sergio Custodio <sergio.custodio@Digital-Grenoble.local>
2018-02-20 16:08:34 +0100

    table ventes field users_id-> unsigned()

--- database/migrations/2018_01_25_093003_create_ventes_table.php
+++ database/migrations/2018_01_25_093003_create_ventes_table.php
@@ -19,7 +19,7 @@ class CreateVentesTable extends Migration
             $table->integer('boisson_id')->unsigned()->index();
             $table->foreign('boisson_id')->references('id')->on('boissons');
             $table->integer('price');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
